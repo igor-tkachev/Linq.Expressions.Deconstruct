1a517b4d66e741831af8c09222549f0d331e50b9
Igor Tkachev <igor.tkachev@gmail.com>
2025-07-16 21:32:39 -0400

    Update project structure.

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,5 @@
-cd /d "%~dp0"
-"%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe" Linq.Expressions.Deconstruct.sln /p:Configuration=Release /t:Restore;Rebuild /v:m
-"%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe" Linq.Expressions.Deconstruct.sln /p:Configuration=Debug   /t:Restore;Rebuild /v:m
+﻿cd /d "%~dp0"
+dotnet restore
+dotnet build --no-restore
+dotnet test --no-build --verbosity normal
+
